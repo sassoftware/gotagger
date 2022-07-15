ec2d1508d5d754494775fc5a4d6a3f58a2baf2d2
Walter Scheper <Walter.Scheper@sas.com>
2022-07-15 10:53:22 -0400

    fix(git): versioning the initial commit
    Internally, `gotagger` was extracting the HEAD commit in an obtuse way
    that also broke when HEAD was the first commit.

--- internal/git/git_test.go
+++ internal/git/git_test.go
@@ -82,17 +82,26 @@ func TestHead(t *testing.T) {
 	testutils.SimpleGitRepo(t, repo, path)
 
 	r, err := New(path)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 
-	c, err := r.Head()
-	if err != nil {
-		t.Errorf("Head() returned an error: %v", err)
+	if c, err := r.Head(); assert.NoError(t, err, "Head() returned an error") {
+		got, want := c.Message(), "feat: bar\n\nThis is a great bar."
+		assert.Equal(t, want, got)
 	}
+}
 
-	if got, want := c.Message(), "feat: bar\n\nThis is a great bar."; got != want {
-		t.Errorf("Head() returned %q, want %q", got, want)
+func TestHead_one_commit(t *testing.T) {
+	repo, path, teardown := testutils.NewGitRepo(t)
+	defer teardown()
+
+	testutils.CommitFile(t, repo, path, "foo.txt", "chore: initial commit", []byte("foo\n"))
+
+	r, err := New(path)
+	require.NoError(t, err)
+
+	if c, err := r.Head(); assert.NoError(t, err, "Head() returned an error") {
+		got, want := c.Message(), "chore: initial commit"
+		assert.Equal(t, want, got)
 	}
 }
 
